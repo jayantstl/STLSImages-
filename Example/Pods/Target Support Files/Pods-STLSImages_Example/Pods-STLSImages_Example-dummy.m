d39c45a68befa2065372a6269d9ac42eb7d943bc
iAdev <jayant.bellway@gmail.com>
2020-12-14 17:54:49 +0530

    Initial commit

---
+++ Example/Pods/Target Support Files/Pods-STLSImages_Example/Pods-STLSImages_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_STLSImages_Example : NSObject
+@end
+@implementation PodsDummy_Pods_STLSImages_Example
+@end
---
+++ Example/Pods/Target Support Files/STLSImages/STLSImages-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_STLSImages : NSObject
+@end
+@implementation PodsDummy_STLSImages
+@end
